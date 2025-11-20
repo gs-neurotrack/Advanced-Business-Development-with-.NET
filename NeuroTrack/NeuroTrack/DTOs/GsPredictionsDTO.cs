0a2b704fad1e2a47840d82cd1bf4f77c252ab297
Cleytonrik99 <enriquecleyton@gmail.com>
2025-11-20 13:26:59 -0300

    Gs Predictions DTO

---
+++ NeuroTrack/NeuroTrack/DTOs/GsPredictionsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace NeuroTrack.DTOs;
+
+public record GsPredictionsDTO(
+    long IdPrediction,
+    float StressPredicted,
+    string Message,
+    DateTime DatePredicted,
+    long IdUser,
+    long IdScores,
+    long IdStatusRisk);
